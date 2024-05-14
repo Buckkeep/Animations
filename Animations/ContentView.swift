7697cc0ea0a4727292b54c2c0dedd0115386c428
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-14 16:07:07 +0100

    Animating drag gesture (explicit)

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -22,10 +22,12 @@ struct ContentView: View {
             .gesture(
                 DragGesture()
                     .onChanged { dragAmount = $0.translation }
-                    .onEnded { _ in dragAmount = .zero }
-            
+                    .onEnded { _ in
+                        withAnimation(.bouncy) {
+                            dragAmount = .zero
+                        }
+                    }
             )
-            .animation(.bouncy, value: dragAmount)
     }
 }
 
