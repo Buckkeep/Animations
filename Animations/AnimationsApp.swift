9e4a68071f200f6db5ded630650fa6e554250710
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-14 13:46:56 +0100

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
